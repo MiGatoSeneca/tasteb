7455f8420edbe6bfb2b8dd032d816d85d6114227
MiGatoSeneca <pablo@citious.com>
2017-01-29 16:59:37 +0100

    First Commit

---
+++ routes/img/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+var test_precent = 0.5;
+var test_a_count = 0;
+var test_b_count = 0;
+
+/* GET home page. */
+router.get('/:username/:filename', function(req, res, next) {
+  var username = req.params.username;
+  var filename = req.params.filename;
+  var img_path = 'tests/'+username+'/'+filename;
+
+  var cookie = req.cookies["cf_testab_img_"+username+"_"+filename];
+  if (cookie === undefined){
+    if (test_a_count==0){
+      test_a_count++;
+      cookie = 'a';
+    }else{
+      if((test_b_count/(test_a_count+test_b_count)) <= test_precent){
+        test_b_count++;
+        cookie = 'b';
+      }else{
+        test_a_count++;
+        cookie = 'a';
+      }
+    }
+    res.cookie("cf_testab_img_"+username+"_"+filename,cookie, { maxAge: 900000, httpOnly: true });
+    console.log("Need some cookies: "+cookie);
+  }else{
+    console.log("I have a cookie: "+cookie);
+  }
+  img_path+='_'+cookie;
+  res.sendFile(path.resolve(img_path));
+});
+
+module.exports = router;
