ca1f384efd93d5830c4084a832012271ced76970
MiGatoSeneca <pablo@citious.com>
2017-01-30 13:12:30 +0100

    update

--- routes/ab/index.js
+++ routes/ab/index.js
@@ -8,28 +8,48 @@ var test_b_count = 0;
 
 var tests = {};
 
-/* GET home page. */
-router.get('/:username/img/:filename', function(req, res, next) {
+tests["admin"] = {};
+tests["admin"]["img"]["test.jpg"] = {
+  "variation" : ["a","b"],
+  "count" : 0
+};
+
+
+router.get('/:username/:type/:filename', function(req, res, next) {
   var username = req.params.username;
   var filename = req.params.filename;
-  var img_path = 'tests/'+username+'/img/'+filename;
+  var type = req.params.type;
 
-  if (tests[username] === undefined){
-    tests[username] = {};
-    tests[username][filename] = {
-      "variation" : ["a","b"],
-      "count" : 0
-    };
-  }
-  var cookie = req.cookies["cf_testab_img_"+username+"_"+filename];
+  var cookie = req.cookies["cf_test_"+username+"_"+type+"_"+filename];
   if (cookie === undefined){
-    cookie = tests[username][filename].variation[(tests[username][filename]["count"] % tests[username][filename].variation.length)];
-    tests[username][filename]["count"]++;
-    res.cookie("cf_testab_img_"+username+"_"+filename,cookie, { maxAge: 900000, httpOnly: true });
+    cookie = tests[username][type][filename].variation[(tests[username][type][filename]["count"] % tests[username][type][filename].variation.length)];
+    tests[username][type][filename]["count"]++;
+    res.cookie("cf_test_"+username+"_"+type+"_"+filename, cookie, { maxAge: 900000, httpOnly: true });
   }
-  img_path+='/'+cookie;
-  res.sendFile(path.resolve(img_path));
+  var path = 'tests/'+username+'/'+type+'/'+filename+'/'+cookie;
+  res.sendFile(path.resolve(path));
+
+  //Actualizar Base de datos
 
 });
+
+
+router.get('/:username/:type/:filename/pixel.pxl', function(req, res, next) {
+  var username = req.params.username;
+  var filename = req.params.filename;
+  var type = req.params.type;
+
+  var cookie = req.cookies["cf_test_"+username+"_"+type+"_"+filename];
+  if (cookie !== undefined){
+    var cookie_conversion = req.cookies["cf_test_"+username+"_"+type+"_"+filename+"_conversion"];
+    if (cookie_conversion === undefined){
+      res.cookie("cf_test_"+username+"_"+type+"_"+filename+"_conversion", "true", { maxAge: 900000, httpOnly: true });
+      //Actualizar Base de datos
+    }
+  }
+  //img_path = enviar pixel 1x1 transparente
+  res.sendFile(path.resolve(img_path));
+});
+
 
 module.exports = router;
